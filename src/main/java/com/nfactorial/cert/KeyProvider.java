59970bdf450bee004d3c0aeb6dd169cc246f5aab
Abhishek Nagpure <abhisheknagpure08@gmail.com>
2018-04-20 21:45:28 +0530

    Adding JWT verifier and Public key fetching logic

--- src/main/java/com/nfactorial/cert/KeyProvider.java
+++ src/main/java/com/nfactorial/cert/KeyProvider.java
@@ -1,10 +1,12 @@
 package com.nfactorial.cert;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.Key;
@@ -20,6 +22,13 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import com.nfactorial.auth.constant.AppConstants;
 import sun.security.x509.AlgorithmId;
 import sun.security.x509.CertificateAlgorithmId;
@@ -163,4 +172,35 @@ public Key getPrivateKey(String alias) {
 	    }
 	    return keyStore;
 	}
+	
+	public String getPublicKeyFrmServer(String authServerUrl) {
+		
+		String url=authServerUrl+"/getPublicKey";
+		HttpClient client= HttpClientBuilder.create().build();
+		HttpGet req= new HttpGet(url);
+		
+		StringBuffer result=null;
+		try {
+			HttpResponse response= client.execute(req);
+			int responsecode=response.getStatusLine().getStatusCode();
+			if( responsecode>=200 && responsecode<400) {
+				BufferedReader rd = new BufferedReader(
+						new InputStreamReader(response.getEntity().getContent()));
+				
+				result = new StringBuffer();
+				String line = "";
+				while ((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result.toString();
+	}
+	
 }
